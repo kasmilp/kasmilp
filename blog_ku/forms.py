751e01fe7281c389c6548bcccf966205aef91b70
kasmil papalia <45504012+kasmilp@users.noreply.github.com>
2019-12-31 12:54:23 +0900

    Add files via upload

---
+++ blog_ku/forms.py
@@ -0,0 +1,60 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError #tambahan prak.7
+from blog_ku.models import User
+from flask_login import current_user
+from flask_wtf.file import FileField, FileAllowed
+
+
+class Admin_F(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('submit')
+
+    # def validate_username(self, username):
+    #   if username.data != current_user.username:
+    #       admin = Admin.query.filter_by(username=username.data).first()
+    #       if user:
+    #           raise ValidationError('???')
+
+class Registrasi_F(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=30)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('password', validators=[DataRequired()])
+    konfirmasi_password = PasswordField('konfirmasi password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+#harus dibawah class registrasi_f
+class Update_Account_F(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    foto = FileField('Update Foto Profil', validators=[FileAllowed(['jpg','png','jpeg'])])
+    submit = SubmitField('Update')
+
+
+    #Tambahkan cek username & password sama
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Usename yang anda masukan sudah digunakan, cobalah menggunakan username yang berbeda')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Email yang anda msukan sudah digunakan, cobalah menggunakan email yang berbeda')
+
+
+
+class Login_F(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+class Post_F(FlaskForm):
+    title = StringField('title', validators=[DataRequired()])
+    konten = TextAreaField('konten', validators=[DataRequired()])
+    submit = SubmitField('POST')
---
+++ blog_ku/models.py
@@ -0,0 +1,78 @@
+from datetime import datetime
+from blog_ku import db, login_manager, app
+from flask_login import UserMixin
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
+admin = Admin(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+	return User.query.get(int(user_id))
+
+
+class Admin(db.Model, UserMixin):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(20),nullable=False)
+	password = db.Column(db.String(60), nullable=False)
+
+
+class User(db.Model, UserMixin):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(20), unique=True, nullable=False)
+	email = db.Column(db.String(120), unique=True, nullable=False)
+	image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+	password = db.Column(db.String(60), nullable=False)
+	posts = db.relationship('Post', backref='penulis', lazy=True)
+
+	def __repr__(self):
+		return f"User('{self.username}', '{self.email}', '{self.image_file}', '{self.password}')"
+
+class Post(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(100), nullable=False)
+	tgl_post = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+	konten = db.Column(db.Text, nullable=False)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+	def __repr__(self):
+		return f"Post('{self.title}', '{self.tgl_post}', '{self.konten}')"
+
+
+class Matakuliah(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	matakuliah = db.Column(db.String(20), nullable=False)
+	sks = db.Column(db.Integer, nullable=False)
+	keterangan = db.Column(db.Text, nullable=False)
+	mk = db.relationship('Jadwal', backref='Matakuliah', lazy=True)
+
+	def __repr__(self):
+		return f"Matakuliah('{self.matakuliah}', '{self.sks}')"
+
+class Dosen(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	nama = db.Column(db.String(30), nullable=False)
+	nidn = db.Column(db.Integer, nullable=False)
+	no_telpon = db.Column(db.Integer, nullable=False)
+	alamat = db.Column(db.String(100), nullable=False)
+	dosens = db.relationship('Jadwal', backref='Dosen', lazy=True)
+
+	def __repr__(self):
+		return f"Dosen('{self.nama}', '{self.nidn}')"
+
+class Jadwal(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	hari = db.Column(db.DateTime, nullable=False)
+	jam = db.Column(db.Time, nullable=False)
+	mk_id = db.Column(db.Integer, db.ForeignKey('matakuliah.id'))
+	dosen_id = db.Column(db.Integer, db.ForeignKey('dosen.id'))
+
+	def __repr__(self):
+		return f"Jadwal('{self.hari}', '{self.jam}')"
+
+
+admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Post, db.session))
+admin.add_view(ModelView(Matakuliah, db.session))
+admin.add_view(ModelView(Dosen, db.session))
+admin.add_view(ModelView(Jadwal, db.session))
