a7dda85ada73733f8878c5ee963b0f5282ee7c04
kasmil papalia <45504012+kasmilp@users.noreply.github.com>
2019-12-31 11:54:28 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
+import os
 from blog_ku import app
 
-if __name__=="__main__":
-	app.run (debug=True)
+#class User dan Post sebelumnya dipindah ke models.py
+
+
+if __name__ == '__main__':
+	port = int(os.environ.get("PORT", 5000))
+	app.run(debug=True, host='0.0.0.0', port=port)
